c14349e1e7562d8bf4d0e1ccedce0e0090afb33b
Thomas Huijer <thomas.huijer@evision-software.com>
2015-03-31 13:21:49 +0200

    Added comments to show usage

--- DemoApp/Program.cs
+++ DemoApp/Program.cs
@@ -10,6 +10,10 @@ namespace DemoApp
             using (WebApp.Start<Startup>("http://localhost:12345"))
             {
                 Console.WriteLine("Server is running...");
+                Console.WriteLine("Plain HTML: http://localhost:12345/test");
+                Console.WriteLine("WebApi:     http://localhost:12345/api");
+                Console.WriteLine("Nancy:      http://localhost:12345/site");
+                Console.WriteLine("Nancy:      http://localhost:12345/site/hello");
                 Console.ReadLine();
             }
         }
